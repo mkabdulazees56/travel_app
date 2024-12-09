9ddf186fd73b1f9f2becda1eb4cf7b337f3baf0d
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-12-09 10:49:05 +0530

    destination carousal added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_app/widgets/destination_carousal.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,7 +11,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  int _currentTab = 0;
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
@@ -49,34 +49,34 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView(
-        padding: EdgeInsets.symmetric(vertical: 30.0),
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(left: 20, right: 120),
-            child: const Text(
-              'What would you like to find?',
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 120),
+              child: Text(
+                'What would you like to find?',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: _icons
-                .asMap()
-                .entries
-                .map(
-                  (MapEntry map) => _buildIcon(map.key),
-                )
-                .toList(),
-          ),
-        ],
-      )),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map((MapEntry map) => _buildIcon(map.key))
+                  .toList(),
+            ),
+            SizedBox(height: 20.0),
+            DestinationCarousel(),
+            SizedBox(height: 20.0),
+          ],
+        ),
+      ),
     );
   }
 }
